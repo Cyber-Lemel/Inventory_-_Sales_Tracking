aa6724c8e0ef1b1bd3728df96885b68e1bcf63b1
Mel Joseph Velasco <velascomeljoseph7@gmail.com>
2026-08-02 14:11:35 +0800

    Delete Customer_dashboard directory

--- Customer_dashboard/customer.js
+++
@@ -1,9 +0,0 @@
-let orderProduct = document.querySelectorAll(".orderProduct");
-orderProduct.forEach((button) => {
-    button.addEventListener("click", () => {
-        const productId = button.getAttribute("data-id");
-        if (confirm("Add this product to your order?")) {
-            window.location.href = `orderProduct.php?id=${productId}`;
-        }
-    });
-});
