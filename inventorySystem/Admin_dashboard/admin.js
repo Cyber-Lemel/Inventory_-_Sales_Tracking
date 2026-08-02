15dde485b43472af45e60ceaed7895c59c848495
Mel Joseph Velasco <velascomeljoseph7@gmail.com>
2026-08-02 14:12:02 +0800

    Delete inventorySystem directory

--- inventorySystem/Admin_dashboard/admin.js
+++
@@ -1,9 +0,0 @@
-let acceptButtons = document.querySelectorAll(".acceptProduct");
-acceptButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        const productId = button.getAttribute("data-id");
-        if (confirm("Accept this product? It will become visible to users.")) {
-            window.location.href = `productAccept.php?id=${productId}`;
-        }
-    });
-});
